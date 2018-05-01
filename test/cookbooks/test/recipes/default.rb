7bd70373192367c538a51cc665b9ef8e6a5ef74c
Tim Smith <tsmith@chef.io>
2018-05-01 16:30:45 -0700

    Remove the path setup of choco from the test recipe
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,8 +1,5 @@
 include_recipe 'chocolatey::default'
 
-# this is really a bug in the chocolatey cookbook
-windows_path 'C:\ProgramData\chocolatey\bin'
-
 chocolatey_config 'Set the Chocolatey config path' do
   config_key 'cacheLocation'
   value 'c:\temp\choco'
